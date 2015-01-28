b3d8036c65aedd1e1d992318d86271f70df5136c
Joel Tong <me@joeltong.org>
2015-01-28 12:07:41 -0800

    committing GET routes

--- api/controllers/PastelController.js
+++ api/controllers/PastelController.js
@@ -7,8 +7,17 @@ PastelController
  */
 module.exports = {
   saveColors: function(req, res) {
-    return res.json({
-      colors: [[23, 55, 70], [55, 55, 200], [0, 0, 0], [255, 0, 0], [50, 50, 200]]
-    });
+  	var pastel = {
+  		scheme: [[23, 55, 70], [55, 55, 200], [0, 0, 0], [255, 0, 0], [50, 50, 200]], 
+  		time: Date.now() 
+  	}
+
+  	Pastel.create(pastel).exec(function(err, model) {
+  		if (err) {
+  		} else {
+		    return res.json(pastel);
+  		}
+  	});
   }
+
 };
